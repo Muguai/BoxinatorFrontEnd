ca4bcc07fb211dbd4e01f41b8432cd0314f702ec
Elina Eriksson <ee223mz@student.lnu.se>
2023-09-01 12:22:22 +0200

    style: indication of active link

--- src/app/components/admin/admin-boxes/admin-boxes.component.ts
+++ src/app/components/admin/admin-boxes/admin-boxes.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-boxes',
   templateUrl: './admin-boxes.component.html',
-  styleUrls: ['./admin-boxes.component.scss']
+  styleUrls: ['./admin-boxes.component.scss', '../../../pages/admin/admin.component.scss']
 })
 export class AdminBoxesComponent {
 
