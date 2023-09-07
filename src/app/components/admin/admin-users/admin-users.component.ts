dbf4b4ee753f66afc88fe70956fd758ac8f10159
Elina Eriksson <ee223mz@student.lnu.se>
2023-09-07 16:34:49 +0200

    feat: admin user table

--- src/app/components/admin/admin-users/admin-users.component.ts
+++ src/app/components/admin/admin-users/admin-users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AdminUser } from 'src/app/models/adminUser';
 
 @Component({
   selector: 'app-admin-users',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-users.component.scss']
 })
 export class AdminUsersComponent {
+    // controls which columns to render and in what order
+  public displayedColumns: string[] = ['name', 'mail', 'action'];
+  public users: AdminUser[] = [
+    {id: 1, name: 'John Doe', mail: 'john.doe@mail.com', active: true},
+    {id: 2, name: 'Jane Doe', mail: 'jane.doe@mail.com', active: false}
+  ];
 
+  public toggleActiveState(user: AdminUser): void {
+    user.active = !user.active;
+    // PUT REQUEST HERE
+  }
 }
