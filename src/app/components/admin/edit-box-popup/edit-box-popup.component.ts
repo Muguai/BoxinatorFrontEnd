ece8667b90d6cb26d0fc510f4025e221ecb35610
Elina Eriksson <ee223mz@student.lnu.se>
2023-09-07 14:46:34 +0200

    feat: read values from edit box popup form

--- src/app/components/admin/edit-box-popup/edit-box-popup.component.ts
+++ src/app/components/admin/edit-box-popup/edit-box-popup.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Box, dummyBoxes } from 'src/app/models/mysteryBox';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-box-popup',
@@ -14,5 +15,9 @@ export class EditBoxPopupComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
     this.id = data.id;
     this.box = dummyBoxes[data.id - 1];
+  }
+
+  public onSubmit(form: NgForm): void {
+    console.log(form.value); // VALUES FOR PUT REQUEST
   }
 }
