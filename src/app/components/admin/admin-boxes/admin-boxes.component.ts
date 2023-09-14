ff94052fc59dec9e1802ba5401b0a23bad80261e
Elina Eriksson <ee223mz@student.lnu.se>
2023-09-14 10:13:07 +0200

    feat: validation for edit rate forms and edit box form refactor: removed surprise element inputs from edit box form style: minor style adjustments

--- src/app/components/admin/admin-boxes/admin-boxes.component.ts
+++ src/app/components/admin/admin-boxes/admin-boxes.component.ts
@@ -6,7 +6,7 @@ import { EditBoxPopupComponent } from '../edit-box-popup/edit-box-popup.componen
 @Component({
   selector: 'app-admin-boxes',
   templateUrl: './admin-boxes.component.html',
-  styleUrls: ['./admin-boxes.component.scss', '../../../pages/admin/admin.component.scss']
+  styleUrls: ['./admin-boxes.component.scss']
 })
 export class AdminBoxesComponent {
   // controls which columns to render and in what order
