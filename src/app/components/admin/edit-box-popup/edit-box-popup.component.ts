97adf5f15f2f5b92706166757887a35a4495e0d0
Elina Eriksson <ee223mz@student.lnu.se>
2023-09-18 23:33:14 +0200

    feat: update boxes

--- src/app/components/admin/edit-box-popup/edit-box-popup.component.ts
+++ src/app/components/admin/edit-box-popup/edit-box-popup.component.ts
@@ -2,6 +2,8 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { NgForm } from '@angular/forms';
 import { ReadBoxDTO } from 'src/app/models/DTOs/Box/readBoxDTO';
+import { BoxServiceService } from 'src/app/services/box-service/box-service.service';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 
 @Component({
   selector: 'app-edit-box-popup',
@@ -10,10 +12,24 @@ import { ReadBoxDTO } from 'src/app/models/DTOs/Box/readBoxDTO';
 })
 export class EditBoxPopupComponent {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: ReadBoxDTO) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: ReadBoxDTO,
+  private readonly authService: AuthenticationService,
+  private readonly boxService: BoxServiceService) {}
 
-  onSubmit(form: NgForm): void {
-    // VALUES FOR PUT REQUEST
-    console.log(form.value);
+  async onSubmit(form: NgForm): Promise<void> {
+    this.data.boxName = form.value.boxName;
+    this.data.content = form.value.content;
+    this.data.description = form.value.description;
+    this.data.dimensions = form.value.dimensions;
+    this.data.price = form.value.price;
+    this.data.imageUrl = form.value.imageUrl;
+    this.data.weight = form.value.weight;
+
+    const token = await this.authService.getToken();
+    this.boxService.putBox(token, this.data.id, this.data).subscribe({
+      error: err => {
+        console.error(err);
+      }
+    });
   }
 }
---
+++ src/app/models/DTOs/Box/updateBoxyDTO.ts
@@ -0,0 +1,10 @@
+export interface UpdateBoxDTO {
+    id: number,
+    boxName: string; 
+    description: string;
+    imageUrl: string;
+    price: number;
+    weight: number;
+    content: string;
+    dimensions: string;
+}
