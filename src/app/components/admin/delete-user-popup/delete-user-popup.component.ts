334cc9f206737d39443591133651eaf0a2a3040e
Elina Eriksson <ee223mz@student.lnu.se>
2023-09-19 10:13:05 +0200

    feat: activate/deactivate, make admin/remove admin and delete/restore user

--- src/app/components/admin/delete-user-popup/delete-user-popup.component.ts
+++ src/app/components/admin/delete-user-popup/delete-user-popup.component.ts
@@ -3,6 +3,8 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DeleteUserSnackbarComponent } from '../delete-user-snackbar/delete-user-snackbar.component';
 import { DeleteRestoreUserService } from 'src/app/services/deleteRestoreUser/delete-restore-user.service';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-delete-user-popup',
@@ -11,24 +13,37 @@ import { DeleteRestoreUserService } from 'src/app/services/deleteRestoreUser/del
 })
 export class DeleteUserPopupComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any,
-  private snackBar: MatSnackBar, private readonly deleteRestoreService: DeleteRestoreUserService) {}
+  private snackBar: MatSnackBar, private readonly deleteRestoreService: DeleteRestoreUserService,
+  private readonly authService: AuthenticationService, private readonly userService: UserService) {}
 
-  deleteUser(user: any): void {
-    // DELETE REQUEST - id in data property
-
-    // trigger reload of table over users
-    this.deleteRestoreService.reloadUsers.emit();
+  async deleteUser(user: any): Promise<void> {
+    const token = await this.authService.getToken();
+    this.userService.deleteUser(token, user.id).subscribe({
+      next: () => {
+        // trigger reload of table over users
+        this.deleteRestoreService.reloadUsers.emit();
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
 
     const snackBarRef = this.snackBar.openFromComponent(DeleteUserSnackbarComponent, {
-      duration: 5000,
+      duration: 10000,
       data: user.name
     });
 
-    snackBarRef.onAction().subscribe(() => {
-      // RESTORE REQUEST - id in data property
-
-      // trigger reload of table over users
-      this.deleteRestoreService.reloadUsers.emit();
+    snackBarRef.onAction().subscribe(async () => {
+      const token = await this.authService.getToken();
+      this.userService.restoreUser(token, user.id).subscribe({
+        next: () => {
+          // trigger reload of table over users
+          this.deleteRestoreService.reloadUsers.emit();
+        },
+        error: err => {
+          console.error(err);
+        }
+      });
     });
   }
 }
--- src/app/models/DTOs/Shipment/readShipmentDTO.ts
+++ src/app/models/DTOs/Shipment/readShipmentDTO.ts
@@ -3,7 +3,7 @@ import { Status } from "../../status"
 export interface ReadShipmentDTO {
     id: number,
     created: Date,
-    status: Status,
+    status: Status | null,
     email: string,
     shippingAddress: string,
     billingAddress: string,
@@ -13,4 +13,19 @@ export interface ReadShipmentDTO {
     totalCost: number,
     userId: number,
     countryId: number
+}
+
+export const mockShipment: ReadShipmentDTO = {
+    id: -1,
+    created: new Date(),
+    status: null,
+    email: 'Loading...',
+    shippingAddress: "",
+    billingAddress: "",
+    zipCode: "",
+    instructions: null,
+    giftMessage: null,
+    totalCost: 0,
+    userId: 0,
+    countryId: 0
 }
