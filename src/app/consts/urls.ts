fa0d2cc49066d7cfea66bea587abdf0b4bb4c863
Elina Eriksson <ee223mz@student.lnu.se>
2023-09-21 14:42:59 +0200

    fix: changed url to azure

--- src/app/consts/urls.ts
+++ src/app/consts/urls.ts
@@ -1,3 +1,3 @@
 //For use when testing with localhost
-export const apiBaseUrl = 'http://localhost:5177';
-//export const apiBaseUrl = 'https://boxinator-backend.azurewebsites.net';
+// export const apiBaseUrl = 'http://localhost:5177';
+export const apiBaseUrl = 'https://boxinator-backend.azurewebsites.net';
